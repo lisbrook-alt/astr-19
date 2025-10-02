c710ec1bfab4a9ec908e5cd51fa651800030683d
lisbrook-alt <lisbrook@ucsc.edu>
2025-10-02 12:35:09 -0700

    Session 1

---
+++ session-1-prompts.py
@@ -0,0 +1,16 @@
+#This will print out my name, pronouns
+#and two sentences about my favorite movie and food.
+#While it's not the most efficient I just wanted to
+#get some practice with the material again. So I
+#Made it more complicated to 
+name = 'Full name: Liam Brooks ' #str thats my name
+pronouns = 'He/Him ' #str that is my pronounce
+movie = 'I enjoy The Pitt,\n while not a movie its an award\n winning show that is very well made.\n' #This prints my favorite TV Show. sadly not a movie though
+food = 'Personally I love the chicken surprise\n my mom makes for me every now and then'
+myinfo = name + pronouns + movie + food #
+
+def PrintMyInfo():
+	print(myinfo)
+
+PrintMyInfo()
+
