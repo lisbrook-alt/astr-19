d32aa4be33fc9a387f18ef8c7a4d5869f89bef7b
lisbrook-alt <lisbrook@ucsc.edu>
2025-10-21 12:31:22 -0700

    Add files via upload

---
+++ session-2-prompt.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+i = 2.7483 #creates i, a float
+j = 3.1415926535 #creates j, a float of the first digits of pi
+x = i+j
+y = i-j
+z = i*j
+
+print("i + j = ", x)
+print(type(x))
+print("i - j = ", y)
+print(type(y))
+print("i * j = ", z)
+print(type(z))
